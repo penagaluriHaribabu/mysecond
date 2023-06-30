001deed37d8dba037ee41d45eb43baff0d47888c
unknown <penagalurih@gmail.com>
2023-06-30 11:43:36 +0530

    Commiting Employee and Developers code

---
+++ Developers.java
@@ -0,0 +1,9 @@
+public class Developers {
+    public static void main(String[] args) {
+        System.out.println("No.of Developers are in korber:");
+        System.out.println("Laddu");
+        System.out.println("Bujji");
+        System.out.println("Hari");
+        System.out.println("Loki");
+    }
+}
---
+++ Employee.java
@@ -0,0 +1,9 @@
+public class Employee {
+    public static void main(String[] args) {
+        System.out.println("No.of Employees in Korber:");
+        System.out.println("Hari");
+        System.out.println("Nikil");
+        System.out.println("praveen");
+        System.out.println("swathi");
+    }
+}
